fff0791877b2e3bbd481908cbca172cc65391123
Fangming Lu <cjlfm007@gmail.com>
2019-11-10 22:36:58 -0800

    import drawable image into different folder

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -18,17 +18,17 @@ public class MainActivity extends AppCompatActivity {
 
         // Create an ArrayList of Music objects
         final ArrayList<Music> musicList = new ArrayList<>();
-        musicList.add(new Music("Ard", "2000 Clicks", R.drawable.ard));
-        musicList.add(new Music("Ark", "100 Clicks", R.drawable.ark));
-        musicList.add(new Music("Babysong", "4 Clicks", R.drawable.babysong));
-        musicList.add(new Music("Germansong", "27 Clicks", R.drawable.germansong));
-        musicList.add(new Music("Hq", "51 Clicks", R.drawable.hq));
-        musicList.add(new Music("Jmusic", "200 Clicks", R.drawable.jmusic));
-        musicList.add(new Music("John Dory", "4 Clicks", R.drawable.jorhdory));
-        musicList.add(new Music("Rock", "23 Clicks", R.drawable.rock));
-        musicList.add(new Music("Pop", "2 Clicks", R.drawable.popsong));
-        musicList.add(new Music("Piano", "0 Clicks", R.drawable.piano));
-        musicList.add(new Music("RongHao", "129 Clicks", R.drawable.lironghao));
+        musicList.add(new Music("Ard", "2000 Clicks", R.drawable.song_gene_ard));
+        musicList.add(new Music("Ark", "100 Clicks", R.drawable.song_gene_ark));
+        musicList.add(new Music("Babysong", "4 Clicks", R.drawable.song_gene_babysong));
+        musicList.add(new Music("Germansong", "27 Clicks", R.drawable.song_gene_german));
+        musicList.add(new Music("Hq", "51 Clicks", R.drawable.song_gene_hq));
+        musicList.add(new Music("Jmusic", "200 Clicks", R.drawable.song_gene_jmusic));
+        musicList.add(new Music("John Dory", "4 Clicks", R.drawable.song_gene_jorhdory));
+        musicList.add(new Music("Rock", "23 Clicks", R.drawable.song_gene_rock));
+        musicList.add(new Music("Pop", "2 Clicks", R.drawable.song_gene_popsong));
+        musicList.add(new Music("Piano", "0 Clicks", R.drawable.song_gene_piano));
+        musicList.add(new Music("RongHao", "129 Clicks", R.drawable.song_gene_lironghao));
 
         final MusicListAdapter musicAdapter = new MusicListAdapter(this, musicList);
         ListView listView = findViewById(R.id.music_list_view);
